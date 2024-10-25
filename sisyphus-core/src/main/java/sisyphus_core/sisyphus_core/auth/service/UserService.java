c1cac0072fa3854853c2dd078e9bbdcb52c1a4cc
immyeong <wpfls6670@naver.com>
2024-10-25 14:58:32 +0900

    feat: user active 실시간 전송 로직 구현

--- sisyphus-core/src/main/java/sisyphus_core/sisyphus_core/auth/service/UserService.java
+++ sisyphus-core/src/main/java/sisyphus_core/sisyphus_core/auth/service/UserService.java
@@ -54,8 +54,8 @@ public class UserService {
 
     //유저 회원가입
     @Transactional
-    public void register(UserRequest.register register){
-        if(userRepository.findByNickname(register.getNickname()).isPresent()){
+    public void register(UserRequest.register register) {
+        if (userRepository.findByNickname(register.getNickname()).isPresent()) {
             throw new DuplicateUserException("이미 존재하는 아이디입니다.");
         }
 
@@ -76,7 +76,7 @@ public class UserService {
 
     //로그인
     @Transactional
-    public TokenResponse authenticate(UserRequest.login login, HttpServletResponse response){
+    public TokenResponse authenticate(UserRequest.login login, HttpServletResponse response) {
         CustomUserDetails userDetails = (CustomUserDetails) customUserDetailService.loadUserByUsername(login.getLoginId());
         User user = userRepository.findByLoginId(login.getLoginId()).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
 
@@ -90,35 +90,46 @@ public class UserService {
         Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authentication);
         response.addHeader("Authorization", "Bearer " + accessToken);
-        if(existingToken == null){
+        if (existingToken == null) {
             String refreshToken = jwtUtil.getRefreshToken(userDetails);
             Token token = Token.builder().accessToken(accessToken).refreshToken(refreshToken).loginId(login.getLoginId()).build();
             tokenService.saveToken(token);
-        }else{
+        } else {
             tokenService.updateAccessToken(existingToken.getRefreshToken(), accessToken);
         }
         user.setState(UserState.ACTIVE);
+
+        List<UserFollower> userFollowerList = userFollowerRepository.findByUser(user);
+        for (UserFollower userFollower : userFollowerList) {
+            User followerUser = userFollower.getFollowerUser();
+            template.convertAndSend("/queue/state/" + followerUser.getNickname());
+        }
         return new TokenResponse(accessToken);
     }
 
     //로그아웃
     @Transactional
-    public void unAuthenticate(String loginId, HttpServletResponse response){
+    public void unAuthenticate(String loginId, HttpServletResponse response) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
         SecurityContextHolder.clearContext();
         tokenService.deleteToken(loginId);
         response.setHeader("Authorization", null);
         user.setState(UserState.NONACTIVE);
+        List<UserFollower> userFollowerList = userFollowerRepository.findByUser(user);
+        for (UserFollower userFollower : userFollowerList) {
+            User followerUser = userFollower.getFollowerUser();
+            template.convertAndSend("/queue/state/" + followerUser.getNickname());
+        }
     }
 
     //회원 정보 변경
     @Transactional
-    public void modify(UserRequest.modify modify, String loginId){
+    public void modify(UserRequest.modify modify, String loginId) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 회원이 없습니다."));
         List<UserChatRoom> userChatRoomsByUser = userChatRoomRepository.findUserChatRoomsByUser(user);
         for (UserChatRoom userChatRoom : userChatRoomsByUser) {
             ChatRoom chatRoom = userChatRoom.getChatRoom();
-            if(!chatRoom.isCustomRoomName()){
+            if (!chatRoom.isCustomRoomName()) {
                 String replaceRoomName = chatRoom.getRoomName().replace(user.getNickname(), modify.getNickname());
                 chatRoom.setRoomName(replaceRoomName);
                 chatRoomRepository.save(chatRoom);
@@ -132,7 +143,7 @@ public class UserService {
 
     //회원탈퇴
     @Transactional
-    public void withdrawal(String loginId){
+    public void withdrawal(String loginId) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 회원이 없습니다."));
         List<UserChatRoom> userChatRoomsByUser = userChatRoomRepository.findUserChatRoomsByUser(user);
         tokenService.deleteToken(loginId);
@@ -145,37 +156,37 @@ public class UserService {
 
     //아이디찾기
     @Transactional
-    public UserResponse.find findLoginId(String email){
+    public UserResponse.find findLoginId(String email) {
         User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
         return new UserResponse.find(user.getLoginId(), "");
     }
 
     //비밀번호찾기
     @Transactional
-    public UserResponse.find findPassword(String loginId){
+    public UserResponse.find findPassword(String loginId) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
         return new UserResponse.find("", user.getPassword());
     }
 
     //유저 로그인 아이디 중복 검증
     @Transactional
-    public void checkDuplicateLoginId(String loginId){
-        if(userRepository.findByLoginId(loginId).isPresent()){
+    public void checkDuplicateLoginId(String loginId) {
+        if (userRepository.findByLoginId(loginId).isPresent()) {
             throw new DuplicateUserLoginIdException("이미 존재하는 아이디입니다.");
         }
     }
 
     //유저 닉네임 중복 검증
     @Transactional
-    public void checkDuplicateNickname(String nickname){
-        if(userRepository.findByNickname(nickname).isPresent()){
+    public void checkDuplicateNickname(String nickname) {
+        if (userRepository.findByNickname(nickname).isPresent()) {
             throw new DuplicateUserNicknameException("이미 존재하는 닉네임입니다.");
         }
     }
 
     //팔로우
     @Transactional
-    public void follow(String loginId, String nickname){
+    public void follow(String loginId, String nickname) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가없습니다."));
         User followUser = userRepository.findByNickname(nickname).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
 
@@ -187,7 +198,7 @@ public class UserService {
 
     //언팔로우
     @Transactional
-    public void unfollow(String loginId, String nickname){
+    public void unfollow(String loginId, String nickname) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가없습니다."));
         User followUser = userRepository.findByNickname(nickname).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
 
@@ -197,7 +208,7 @@ public class UserService {
 
     //팔로워유저 조회
     @Transactional
-    public List<UserResponse> findFollowerList(String loginId){
+    public List<UserResponse> findFollowerList(String loginId) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
         List<UserFollower> userFollowerList = userFollowerRepository.findByUser(user);
         List<User> followerList = userFollowerList.stream().map(UserFollower::getFollowerUser).collect(Collectors.toList());
@@ -206,7 +217,7 @@ public class UserService {
 
     //팔로잉유저 조회
     @Transactional
-    public List<UserResponse> findFollowingList(String loginId){
+    public List<UserResponse> findFollowingList(String loginId) {
         User user = userRepository.findByLoginId(loginId).orElseThrow(() -> new UsernameNotFoundException("일치하는 유저가 없습니다."));
         List<UserFollowing> userFollowingList = userFollowingRepository.findByUser(user);
         List<User> followingList = userFollowingList.stream().map(UserFollowing::getFollowingUser).collect(Collectors.toList());
@@ -215,25 +226,25 @@ public class UserService {
 
     //모든 유저 조회
     @Transactional
-    public List<UserResponse> findAllUser(){
+    public List<UserResponse> findAllUser() {
         List<User> allUser = userRepository.findAll();
         return toUserResponse(allUser);
     }
 
     //테스트 코드용 데이터 전체 삭제
     @Transactional
-    public void deleteAll(){
+    public void deleteAll() {
         userFollowerRepository.deleteAll();
         userFollowingRepository.deleteAll();
         userRepository.deleteAll();
     }
 
     @Transactional
-    public User findByNickname(String nickname){
+    public User findByNickname(String nickname) {
         return userRepository.findByNickname(nickname).get();
     }
 
-    protected List<UserResponse> toUserResponse(List<User> users){
+    protected List<UserResponse> toUserResponse(List<User> users) {
         List<UserResponse> userResponseList = new ArrayList<>();
         for (User user : users) {
             UserResponse userResponse = UserResponse.builder()
