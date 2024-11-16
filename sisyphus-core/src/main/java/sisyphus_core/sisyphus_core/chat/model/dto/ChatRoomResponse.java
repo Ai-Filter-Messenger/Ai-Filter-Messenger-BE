98fc7660f0c538717c56c701a66feeaacca879dc
Lee Ye Jin <jene0028@gmail.com>
2024-11-17 03:13:27 +0900

    feat: customRoomName 필드 추가, isCustomRoomName 상태 반환

--- sisyphus-core/src/main/java/sisyphus_core/sisyphus_core/chat/model/dto/ChatRoomResponse.java
+++ sisyphus-core/src/main/java/sisyphus_core/sisyphus_core/chat/model/dto/ChatRoomResponse.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sisyphus_core.sisyphus_core.auth.model.dto.UserResponse;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Getter
@@ -16,6 +18,22 @@ public class ChatRoomResponse {
     private Long chatRoomId;
     private ChatRoomType type;
     private String roomName;
-    private List<String> profileImages;
+    private List<UserResponse.toChat> userInfo;
     private int userCount;
+    private String recentMessage;
+    private ZonedDateTime createAt;
+    private boolean isFix;
+    private int NotificationCount;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class OpenChatRoom {
+        private String roomName;
+        private String chatroomImage;
+        private ZonedDateTime createAt;
+        private int userCount;
+        private String recentMessage;
+    }
 }
